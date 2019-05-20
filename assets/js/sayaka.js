830bad25ccccdc48c62b98a7bfcf2e977ebf7241
sayakaw <syk_hnsr@yahoo.co.jp>
2019-05-21 08:52:59 +0900

    fixed image issue and added js functions

--- assets/js/sayaka.js
+++ assets/js/sayaka.js
@@ -1,14 +1,19 @@
 $(function(){
     
-    
+    // icon x がクリックされたら、menu-title-showのpタグを表示する
     $('.iconx').click(function(){
         $('.menu-title-show').slideDown('slow');
 
-        
-        
 
+    // slidedownのセクションからマウスが離れたら、
+    // menu-title-showのPタグを非表示にする
+          
 
-        
+    })
 
+    $('.slidedown').mouseleave(function(){
+
+        $(this).find('.menu-title-show').slideUp();
+   
     })
 });
